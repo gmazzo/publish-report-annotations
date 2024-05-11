dac4f8da65295a1ddf1a4c35fa75a5543fa9b923
Guillermo Mazzola <gmazzo65@gmail.com>
2024-05-11 16:53:20 +0200

    Improved `resolveFile` performance

--- src/parsers/resolveFile.ts
+++ src/parsers/resolveFile.ts
@@ -4,9 +4,36 @@ import {isAbsolute, relative} from "path";
 import process from "process";
 import {existsSync} from "fs";
 
+const knownLocations: string[] = [];
+
+export function resetCache() {
+    knownLocations.length = 0;
+}
+
 export async function resolveFile(filepath: string, ...possibleExtensions: string[]) {
     if (isAbsolute(filepath) || existsSync(filepath)) {
         return filepath;
+    }
+    // tries to reuse previously resolved location
+    if (knownLocations.length > 0) {
+        for (const location of knownLocations) {
+            if (possibleExtensions.length > 0) {
+                for (const ext of possibleExtensions) {
+                    const candidate = `${location}/${filepath}.${ext}`;
+
+                    if (existsSync(candidate)) {
+                        return candidate;
+                    }
+                }
+
+            } else {
+                const candidate = `${location}/${filepath}`;
+
+                if (existsSync(candidate)) {
+                    return candidate;
+                }
+            }
+        }
     }
 
     const paths = possibleExtensions.length > 0 ?
@@ -18,6 +45,11 @@ export async function resolveFile(filepath: string, ...possibleExtensions: strin
     if (file) {
         const relativePath = relative(process.cwd(), file);
         core.debug(`File \`${relativePath}\` found for \`${filepath}\` with possible extensions ${possibleExtensions.join(', ')}`);
+
+        const location = relative(process.cwd(), file.substring(0, file.indexOf(filepath)));
+        core.debug(`Adding to known locations: ${location}`);
+        knownLocations.push(location);
+
         return relativePath;
     }
     if (core.isDebug()) {
