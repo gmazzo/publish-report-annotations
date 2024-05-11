553cbf026309b45a7fbb5cbb8f13097ce1d5e3be
Guillermo Mazzola <gmazzo65@gmail.com>
2024-05-11 16:55:04 +0200

    fix: Improved `resolveFile` performance (#20)

--- src/parsers/resolveFile.test.ts
+++ src/parsers/resolveFile.test.ts
@@ -1,9 +1,13 @@
 import * as glob from "@actions/glob";
-import {resolveFile} from "./resolveFile";
+import {resetCache, resolveFile} from "./resolveFile";
 
 jest.spyOn(glob, 'create');
 
 describe("resolveFile", () => {
+
+    beforeEach(() => {
+        resetCache();
+    });
 
     test("when is absolute path, just returns itself", async () => {
         const resolvedPath = await resolveFile("/absolute/path/file.txt");
@@ -38,6 +42,15 @@ describe("resolveFile", () => {
 
         expect(resolvedPath).toBe("org/test/sample/SampleTestSuite");
         expect(glob.create).toHaveBeenCalled();
+    });
+
+    test("when location is found, it can be used without globbing again", async () => {
+        const path1 = await resolveFile("org/test/sample/SampleTestSuite", 'java', 'kt', 'groovy');
+        const path2 =  await resolveFile("org/test/sample/AnotherTestSuite.kt");
+
+        expect(path1).toBe("sample-gradle/src/test/kotlin/org/test/sample/SampleTestSuite.kt");
+        expect(path2).toBe("sample-gradle/src/test/kotlin/org/test/sample/AnotherTestSuite.kt");
+        expect(glob.create).toHaveBeenCalledTimes(1);
     });
 
 });
