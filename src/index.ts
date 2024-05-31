2cf48f11fa41c8fdfff523e9c3059aa312280deb
Guillermo Mazzola <gmazzo65@gmail.com>
2024-05-31 10:21:32 +0200

    fix: Catch promises errors and report them with `.setFailed`

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import * as core from "@actions/core";
 import main from "./main";
 
-(async function run() {
-    try {
-        await main()
-
-    } catch (error) {
-        if (error instanceof Error) core.setFailed(error)
-        else core.setFailed(JSON.stringify(error))
-    }
-})()
+main().catch(core.setFailed);
