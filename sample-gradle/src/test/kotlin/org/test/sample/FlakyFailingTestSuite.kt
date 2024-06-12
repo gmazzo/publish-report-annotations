6557169b6206ef6ea7e108e45afc98ffbe041f21
Guillermo Mazzola <gmazzo65@gmail.com>
2024-06-12 18:01:13 +0200

    Added flaky tests (retry) support

--- sample-gradle/src/test/kotlin/org/test/sample/FlakyFailingTestSuite.kt
+++ sample-gradle/src/test/kotlin/org/test/sample/FlakyFailingTestSuite.kt
@@ -5,14 +5,11 @@ import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.fail
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class FlakyTestSuite {
+class FlakyFailingTestSuite {
 
-    private var times = 0
-
-    @RepeatedTest(5, name = "flakyTest")
-    fun flakyTest() {
-        assertTrue(++times >= 3)
+    @RepeatedTest(3, name = "failingTest")
+    fun failingTest() {
+        fail("this test always fails")
     }
 
 }
