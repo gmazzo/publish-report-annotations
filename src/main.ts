556abf634bc0e53d8f494e4b3ae3fd2f363af1c1
Guillermo Mazzola <gmazzo65@gmail.com>
2024-07-09 00:26:56 +0200

    fix: Reporting link to check summary in logs

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ export default async function main() {
 
     if (files.length > 0) {
         core.notice(`Processed ${files.length} files: ${summaryOf(all)}` +
-            (checkHtmlUrl ? `\nCheck \`${config.checkName}\` reported at ${checkHtmlUrl}` : ''));
+            (checkHtmlUrl ? `.\nSee \`${config.checkName}\` (${checkHtmlUrl})` : ''));
     } else {
         (config.failIfNoReportsFound ? core.setFailed : core.warning)(`No files found to process matching: ${config.reports.join(', ')}`);
     }
