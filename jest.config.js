1a9108b47e9e9b11f7737c29a487e5a79c18299a
Guillermo Mazzola <gmazzo65@gmail.com>
2025-09-16 12:40:24 +0200

    Increasing timeout for tests

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts', '!**/*.it.test.ts'],
-    clearMocks: true,
-    reporters: [
-        "default",
-        [ "jest-junit", { outputFile: 'lib/jest-junit.xml' } ],
-    ],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/*.test.ts", "!**/*.it.test.ts"],
+  clearMocks: true,
+  testTimeout: 15000,
+  reporters: ["default", ["jest-junit", { outputFile: "lib/jest-junit.xml" }]],
 };
